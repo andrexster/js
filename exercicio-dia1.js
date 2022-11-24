f6b3010291c8eb1966237899df3cf6156f574a1c
andrexster <andrexster@debian>
2022-11-24 07:59:22 -0300

    if/else

---
+++ exercicio-dia1.js
@@ -0,0 +1,10 @@
+const nota = 100;
+
+if (nota => 80)
+    console.log('Passou Viado!')
+
+else if(nota < 80 || nota >= 60)
+    console.log('iiiiiiii, Ta na lista de espera')
+
+else if(nota > 60)
+    console.log('Já era viado, só ano que vem')
---
+++ scripts.js
@@ -0,0 +1,5 @@
+    const myName = 'André';
+    const birthCity = 'Nárnia';
+    let birthYear = 1986;
+
+    console.log(myName, birthCity, birthYear);
